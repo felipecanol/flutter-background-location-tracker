5235f0e27a91cb269ff5fbbbd4cc463184c635fe
Felipe Cano <felipecanol@gmail.com>
2024-10-11 10:10:11 -0500

    - Fix: Cannot Access 'lifecycle' error - Gradle was updated - flutter_local_notifications was removed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:math';
 
 import 'package:background_location_tracker/background_location_tracker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -87,11 +85,6 @@ class _MyAppState extends State<MyApp> {
                     MaterialButton(
                       child: const Text('Request Notification permission'),
                       onPressed: _requestNotificationPermission,
-                    ),
-                    MaterialButton(
-                      child: const Text('Send notification'),
-                      onPressed: () =>
-                          sendNotification('Hello from another world'),
                     ),
                     MaterialButton(
                       child: const Text('Start Tracking'),
@@ -203,7 +196,6 @@ class Repo {
   Future<void> update(BackgroundLocationUpdateData data) async {
     final text = 'Location Update: Lat: ${data.lat} Lon: ${data.lon}';
     print(text); // ignore: avoid_print
-    sendNotification(text);
     await LocationDao().saveLocation(data);
   }
 }
@@ -240,30 +232,4 @@ class LocationDao {
   }
 
   Future<void> clear() async => (await prefs).clear();
-}
-
-void sendNotification(String text) {
-  const settings = InitializationSettings(
-    android: AndroidInitializationSettings('app_icon'),
-    iOS: IOSInitializationSettings(
-      requestAlertPermission: false,
-      requestBadgePermission: false,
-      requestSoundPermission: false,
-    ),
-  );
-  FlutterLocalNotificationsPlugin().initialize(
-    settings,
-    onSelectNotification: (data) async {
-      print('ON CLICK $data'); // ignore: avoid_print
-    },
-  );
-  FlutterLocalNotificationsPlugin().show(
-    Random().nextInt(9999),
-    'Title',
-    text,
-    const NotificationDetails(
-      android: AndroidNotificationDetails('test_notification', 'Test'),
-      iOS: IOSNotificationDetails(),
-    ),
-  );
 }
